901636bbd1acc92a6471349b0e835817780c2e23
Sauljg18 <L20530284@cancun.tecnm.mx>
2024-09-13 12:52:24 -0500

    actualización tabla cliente registrado snapshot 5cgqs89

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ let conexion = mysql.createConnection({
     password: ""
 })
 
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 app.set("view engine", "ejs");
 
 //se utiliza para manejar datos
