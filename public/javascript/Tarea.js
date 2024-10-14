90238c6e672d8e43d11b6361c4a68d013bf253e6
Sauljg18 <L20530284@cancun.tecnm.mx>
2024-10-14 16:49:00 -0500

    tabla equipo completo, fondo, tareas, 23049jfc

--- public/javascript/Tarea.js
+++ public/javascript/Tarea.js
@@ -1,5 +1,8 @@
 window.onload = function() {
   const finalizarBtns = document.querySelectorAll('.finalizar-btn');
+  const reactivarbtns = document.querySelectorAll('.reactivar-btn')
+
+ 
 
   finalizarBtns.forEach(function(btn) {
     btn.addEventListener('click', function() {
@@ -15,4 +18,22 @@ window.onload = function() {
       })
     })
   })
+
+  reactivarbtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      const taskId = btn.getAttribute('data-id');
+      
+      // Enviar la solicitud POST para actualizar el estado
+      fetch('/reactivar-tarea', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id_tarea: taskId })
+      })
+    })
+  })
+
 }
+
+
