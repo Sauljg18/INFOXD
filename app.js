9736b93bb6de216e0f3826b28a7146c91f0a590e
Sauljg18 <L20530284@cancun.tecnm.mx>
2024-10-17 17:02:58 -0500

    bug login reparado

--- app.js
+++ app.js
@@ -65,17 +65,27 @@ app.get("/", (req, res) => {
 //Verificacion entre ejs loggin y BD
 app.post('/login', (req, res) => {
     const { usuario, contrasena } = req.body;
+
+    // Consulta para verificar el usuario y la contraseña
     const query = `SELECT * FROM colaboradores WHERE usuario = ? AND contrasena = ?`;
+
     connection.query(query, [usuario, contrasena], (err, results) => {
         if (err) {
-            console.error('Error en la BD:', err);
-            return res.status(500).json({ success: false, error: 'Error interno del servidor' }); // JSON con error 500
+            console.error('Error al consultar la base de datos:', err);
+            return res.status(500).send('Error interno del servidor');
         }
-        if (results.length > 0) {
+        // Si se encuentra un colaborador, redirige a una nueva página
+        if (results.length > 0){
             req.session.usuario = results[0];
-            res.json({ success: true }); // Login exitoso
+            connection.query('SELECT DISTINCT colaboradores.nombre AS colaboradorNombre, tabcliente.nombre AS clienteNombre, tabcliente.codigoext AS clientecodigo, tablaequipos.Nombre AS equipoNombre FROM colaboradores, tabcliente, tablaequipos  ', (error, results) => {
+                if (error) {
+                    throw error;
+                } else {
+                    res.render('inicio',  { results: results });
+                }
+            });
         } else {
-            res.status(401).json({ success: false, error: 'Usuario o contraseña incorrectos' }); // Error de autenticación
+            res.send('Usuario o contraseña incorrectos');
         }
     });
 });
