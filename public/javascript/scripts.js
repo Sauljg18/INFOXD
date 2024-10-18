6fdd09a93b7f0f20470b64ece4753d851ed12eb4
Rafael-Ramos-Garcia <rrg14184@gmail.com>
2024-10-18 13:49:16 -0500

    Login Scripst password

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,4 +1,21 @@
+
+
+
 const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
 
-toggle.addEventListener('clik', () => nav.classList.toggle('active'))
+//toggle.addEventListener('clik', () => nav.classList.toggle('active'))
+let viewPassword=document.getElementById('viewPassword');
+let password=document.getElementById('password');
+
+let click = false;
+
+viewPassword.addEventListener('click', (e)=>{
+    if(!click){
+        password.type = 'text'
+        click = true
+    }else if(click){
+        password.type = 'password'
+        click = false
+    }
+})
