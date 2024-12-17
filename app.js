24c985532aed5b055d434a48a49c8e7520ae870e
Sauljg18 <L20530284@cancun.tecnm.mx>
2024-12-17 15:08:07 -0500

    mejoras equipo

--- app.js
+++ app.js
@@ -909,19 +909,22 @@ app.post('/reactivar-tarea', (req, res) => {
     return res.json({ success: true });
     });
 });
+
+
 //ruta de archivos estáticos
 app.use('/resources', express.static("public"));
+
 const PORT = process.env.PORT || 3000;
 // Cambia a la IP de tu servidor o deja 0.0.0.0 para aceptar cualquier conexión
-const HOST = '192.168.0.10';
-/*
+const HOST = '192.168.100.21';
+
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
 });
-*/
 
 
 
+/*
 app.listen(3000,function(){
     console.log("Servidor creado http://localhost:3000");
-});;
+});;*/
